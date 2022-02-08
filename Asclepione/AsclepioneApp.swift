319076a0825489662376e116038abb1401adc2fc
Carkzis <marc.jowett@gmail.com>
2022-02-08 17:21:17 +0000

    Initial Commit

---
+++ Asclepione/AsclepioneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsclepioneApp.swift
+//  Asclepione
+//
+//  Created by Marc Jowett on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsclepioneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
